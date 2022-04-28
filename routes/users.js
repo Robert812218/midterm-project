63f8ad4e86ebb0b96629f074e5112407400775b9
Harjas101 <anand.harjas@gmail.com>
2022-04-28 11:11:48 -0400

    added api function

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
-
+const axios = require("axios")
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
@@ -21,5 +23,74 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  router.get("/add_task", (req, res) => {
+    const newTask = req.body
+    console.log("this is my task", newTask)
+    res.send("hello")
+  });
+  router.post("/add_task", (req, res) => {
+    const {item} = req.body
+
+
+
+
+//api for food start type ingredient/recipe
+
+const options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
+  params: {prefix: item},
+  headers: {
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+    'X-RapidAPI-Key': '0a541a7215mshcf5407821f35232p1d7358jsn1173d3a79f29'
+  }
+};
+
+axios.request(options).then(function (response) {
+//	console.log(response.data);
+  response.data.results.forEach((response)=>{
+    console.log(response.type)
+    if(response.type !== "ingredient"){
+    } else {
+      const productItem = `
+      <li class="list-group-item ">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          ${item}
+        </label>
+      </li>
+      `
+
+    }
+  })
+}).catch(function (error) {
+	console.error(error);
+});
+//api for food end
+
+    res.send("hello")
+  });
+
   return router;
 };
+
+//api for movie start movie/show
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://mdblist.p.rapidapi.com/',
+//   params: {s: 'power'},
+//   headers: {
+//     'X-RapidAPI-Host': 'mdblist.p.rapidapi.com',
+//     'X-RapidAPI-Key': '0a541a7215mshcf5407821f35232p1d7358jsn1173d3a79f29'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+//api for movie end
