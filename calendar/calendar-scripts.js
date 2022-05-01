21e4ce96361568843c4d641ee5f923365d3f5896
Robert812218 <robert812@shaw.ca>
2022-05-01 18:19:54 +0000

    update calendar

--- calendar/calendar-scripts.js
+++ calendar/calendar-scripts.js
@@ -1,32 +1,41 @@
 $(document).ready(function() {
   const date = new Date();
-  const today = date.getDate();
-  const currentMonth = date.getMonth();
+  // const today = date.getDate();
+  // const currentMonth = date.getMonth();
+  
+  $('.month').click(function(){
+    renderMonth(this.id, date);
+  })
 });
 
-function form_calendar(time) {  
+function renderMonth(month, date) {
+  $(".dates-container").empty();
+  let start = arr[1];
+  let len = start + arr.length;
+  let cal = $("dates-calendar");
+  let count = 1;
   
-};
+  for (let i = start; i < len; i++) {
+    cal[i].append(count);
+    count++;
+  }
+}
 
-const calendarLayout = function() {
-  
-};
-
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+// arr: [numberDays, startIndex];
+const months = {
+  January: [31, 6],
+  February: [28, 2],
+  March: [30, 2],
+  April: [30, 5],
+  May: [31, 0],
+  June: [30, 3],
+  July: [31, 5],
+  August: [31, 1],
+  September: [30, 4],
+  October: [31, 6],
+  November: [30, 2],
+  December: [31, 4],
+}
 
 const days = [
   "Sunday",
@@ -37,3 +46,18 @@ const days = [
   "Friday",
   "Saturday"
 ];
+
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December"
+// ];
