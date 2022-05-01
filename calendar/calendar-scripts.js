798d0fe2c91e6afa331005fb8d4953f3ab11a18e
Robert812218 <robert812@shaw.ca>
2022-05-01 03:55:27 +0000

    setup calendar folder/started rendering scripts

---
+++ calendar/calendar-scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  const date = new Date();
+  const today = date.getDate();
+  const currentMonth = date.getMonth();
+});
+
+function form_calendar(time) {  
+  
+};
+
+const calendarLayout = function() {
+  
+};
+
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
