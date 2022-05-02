b234f1dcd06e9faff2d1fde7c3534a4cea6083ba
Robert812218 <robert812@shaw.ca>
2022-05-02 00:20:41 +0000

    updated calendar to render proper number of days per month

--- calendar/calendar-scripts.js
+++ calendar/calendar-scripts.js
@@ -1,63 +1,63 @@
 $(document).ready(function() {
   const date = new Date();
-  // const today = date.getDate();
-  // const currentMonth = date.getMonth();
+  const today = date.getDate();
+  const currentMonth = date.getMonth();
+  console.log(`currentMonth: ${currentMonth}\nCurrent Day: ${today}`);
+  let arr = months[currentMonth];
+  addDates(arr);
   
+  $('#dates-container[today]').css("background-color", "red");
+   
+   $('#January').click(function(){
+    $('.days-of-week').css("background-color", "red");
+    })
   $('.month').click(function(){
-    renderMonth(this.id, date);
+    addDates(arr);
+    console.log(`month clicked`);
+    // figure out how to make background of current date change color
   })
 });
 
-function renderMonth(month, date) {
-  $(".dates-container").empty();
-  let start = arr[1];
-  let len = start + arr.length;
-  let cal = $("dates-calendar");
+function addDates(conditions) {
+  let start = conditions[0];
+  let mid = conditions[1];
+  let end = conditions[2];
+  let newArr = [];
   let count = 1;
+  let cont = $('#dates-container');
+  let blank = `<div>X</div>`;
   
-  for (let i = start; i < len; i++) {
-    cal[i].append(count);
+  
+  do {
+    cont.append(blank);
+    start--;
+  } while (start > 0);
+  
+  do {
+    let block = `<div>${count}</div>`;
+    cont.append(block);
     count++;
-  }
+  } while (count < mid);
+  
+  do {
+    cont.append(blank);
+    end--;
+  } while (end > 0);
 }
 
-// arr: [numberDays, startIndex];
+// arr: [blankBefore, startIndex, blankAfter];
 const months = {
-  January: [31, 6],
-  February: [28, 2],
-  March: [30, 2],
-  April: [30, 5],
-  May: [31, 0],
-  June: [30, 3],
-  July: [31, 5],
-  August: [31, 1],
-  September: [30, 4],
-  October: [31, 6],
-  November: [30, 2],
-  December: [31, 4],
+  0: [6, 31, 5],
+  1: [2, 28, 5],
+  2: [2, 30, 3],
+  3: [5, 30, 0],
+  4: [0, 31, 11],
+  5: [3, 30, 2],
+  6: [5, 31, 6],
+  7: [1, 31, 10],
+  8: [4, 30, 8],
+  9: [6, 31, 5],
+  10: [2, 30, 3],
+  11: [4, 31, 7],
 }
 
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-
-// const months = [
-//   "January",
-//   "February",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "September",
-//   "October",
-//   "November",
-//   "December"
-// ];
