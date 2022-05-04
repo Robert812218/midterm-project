1173b31a2425a0ef3ce9447abab2181f0754a3fe
Robert812218 <robert812@shaw.ca>
2022-05-04 15:15:30 +0000

    problems with the rendering function so just adding this as is for now

--- calendar/calendar-scripts.js
+++ calendar/calendar-scripts.js
@@ -8,10 +8,24 @@ $(document).ready(function() {
   
   $("button").click(function() {
     let item = findMonth(this.id);
-    console.log(item);
+    // console.log(item);
+    // let item = findMonth($(this).text());
+    // console.log(item);
+  });
+  $(".month-previous").click(function() {
+    // console.log($(this).text());
+    // console.log(this);
+    for (const i in months) {
+      console.log(arr);
+      
+    }
+    
+  });
+  $(".month-next").click(function() {
+    console.log(arr);
   });
   $(".date").click(function() {
-    console.log(this);
+    console.log(`this: ${this}`);
     // let $ting = this;
     // $ting.css('color','red');
   });
@@ -47,9 +61,7 @@ function appendBlock(inp) {
   const date = new Date();
   const today = date.getDate();
   let $block = (inp > 0 ? `<div class="date">
-  <button class="date-button">${inp}</button></div>` : `<div></div>` );
-  // let $block2 = `<div class="red-block">${inp}<div>`;
-  // $('#dates-container').append((inp === today) ? $block2 : $block);
+  <button class="date-button">${inp}</button></div>` : `<div></div>`);
 
   $("#dates-container").append($block);
 };
@@ -59,53 +71,76 @@ function findMonth(inp) {
   let out = 0;
   switch(inp) {
     case "January":
+    case 0:
       $contain.empty();
       addDates(months[0]);
+      currentMonth = months[0];
       break;
     case "February":
+    case 1:
       $contain.empty();
       addDates(months[1]);
+      currentMonth = months[1];
       break;
     case "March":
+    case 2:
       $contain.empty();
       addDates(months[2]);
-      out += 2;
+      currentMonth = months[2];
       break;
     case "April":
+    case 3:
       $contain.empty();
       addDates(months[3]);
+      currentMonth = months[3];
       break;
     case "May":
+    case 4:
       $contain.empty();
-      addDates(months[5]);
+      addDates(months[4]);
+      currentMonth = months[4];
       break;
     case "June":
+    case 5:
       $contain.empty();
-      addDates(months[6]);
+      addDates(months[5]);
+      currentMonth = months[5];
       break;
     case "July":
+    case 6:
       $contain.empty();
-      addDates(months[7]);
+      addDates(months[6]);
+      currentMonth = months[6];
       break;
     case "August":
+    case 7:
       $contain.empty();
-      addDates(months[8]);
+      addDates(months[7]);
+      currentMonth = months[7];
       break;
     case "September":
+    case 8:
       $contain.empty();
-      addDates(months[9]);
+      addDates(months[8]);
+      currentMonth = months[8];
       break;
     case "October":
+    case 9:
       $contain.empty();
-      addDates(months[10]);
+      addDates(months[9]);
+      currentMonth = months[9];
       break;
     case "November":
+    case 10:
       $contain.empty();
-      addDates(months[11]);
+      addDates(months[10]);
+      currentMonth = months[10];
       break;
     case "December":
+    case 11:
       $contain.empty();
-      addDates(months[1]);
+      addDates(months[11]);
+      currentMonth = months[11];
       break;
     default:
       console.log(`error`);
@@ -113,6 +148,13 @@ function findMonth(inp) {
   return out;
 }
 
+function loadDate(month, date) {
+  let $contain = $(".date-schedule-container");
+  let $today = $(".date-schedule");
+  $today.empty();
+  $today.append(`<div class="day-date">MAY ${date}</div>`);
+  // loadTasks();
+}
 
 // arr: [blankBefore, startIndex, blankAfter];
 const months = {
@@ -129,4 +171,3 @@ const months = {
   10: [2, 30, 3],
   11: [4, 31, 7],
 }
-
